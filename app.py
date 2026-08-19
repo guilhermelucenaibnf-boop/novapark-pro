cb1e6aa6a179336784c10d5bd5a3c06d934af5ba
guilhermelucenaibnf-boop <guilhermelucenaibnf@gmail.com>
2026-08-19 16:06:37 -0300

    Fix indentation for talao_em_uso check

--- app.py
+++ app.py
@@ -1096,8 +1096,8 @@ if ja_ativo:
     talao_em_uso = conn.execute(
             "SELECT 1 FROM veiculos WHERE empresa_id=? AND numero_talao=?", (session['empresa_id'], numero_talao)
         ).fetchone() if numero_talao else True
-        if talao_em_uso:
-            numero_talao = gerar_numero_talao(conn)
+    if talao_em_uso:
+    numero_talao = gerar_numero_talao(conn)
 
         conn.execute(
             """INSERT INTO veiculos
